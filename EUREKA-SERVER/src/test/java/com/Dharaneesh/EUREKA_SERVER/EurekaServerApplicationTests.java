86a82193aa16c3f5047c2efdfdae0b6f73d295ce
dharaneesh <rvi13970@gmail.com>
2025-01-28 14:07:17 +0530

    Eureka-server Implementation

---
+++ EUREKA-SERVER/src/test/java/com/Dharaneesh/EUREKA_SERVER/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dharaneesh.EUREKA_SERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
